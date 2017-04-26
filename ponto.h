78ab5566bec4e35f9127e1a6c7644124ef5d00f4
gabrielmcoelho <gabriel_mcoelho@hotmail.com>
2017-04-25 22:51:04 -0300

    Update ponto.h

--- ponto.h
+++ ponto.h
@@ -1,7 +1,7 @@
 #ifndef PONTO_H
 #define PONTO_H
 
-/** @brief A classe Ponto serve para a declaração e manipulação de Pontos no plano cartesiano
+/** @brief A classe Ponto serve para a declaração e manipulação de pontos no plano cartesiano
 * bem como para a obtenção de algumas de suas informações
 *
 * A classe Ponto providencia a declaração, obtenção e exibição das coordenadas x e y de cada ponto. Também permite
@@ -17,34 +17,34 @@ public:
     /** @brief Construtor padrão da classe Ponto
     */
     Ponto();
-    /** @brief setX Atribui valores a coordenada x do Ponto
-    *   @param x1 Valor que será atribuido a coordenada x do Ponto
+    /** @brief Atribui valores à coordenada x do ponto
+    *   @param x1 Valor que será atribuído à coordenada x do ponto
     */
     void setX(float x1);
-    /** @brief setY Atribui valores a coordenada y do Ponto
-    *   @param y1 Valor que será atribuido a coordenada y do Ponto
+    /** @brief Atribui valores à coordenada y do ponto
+    *   @param y1 Valor que será atribuído à coordenada y do ponto
     */
     void setY(float y1);
-    /** @brief setXY Atribui valores às coordenadas x e y da classe Ponto simultaneamente
-    *   @param x1 Valor que será atribuido a coordenada x do Ponto
-    *   @param y1 Valor que será atribuido a coordenada y do Ponto
+    /** @brief Atribui valores às coordenadas x e y do ponto simultaneamente
+    *   @param x1 Valor que será atribuído à coordenada x do ponto
+    *   @param y1 Valor que será atribuído à coordenada y do ponto
     */
     void setXY(float x1, float y1);
-    /** @brief getX recupera o valor de x da classe Ponto
+    /** @brief Recupera o valor da coordenada x do ponto
     *   @return Coordenada x do ponto
-    */ 
+    */
     float getX(void);
-    /** @brief Recupera o valor de y da classe Ponto
+    /** @brief Recupera o valor da coordenada y do ponto
     *   @return Coordenada y do ponto
-    */    
+    */
     float getY(void);
-    /** @brief Executa a adição de pontos
-    *   @param p1 Ponto contendo as coordenadas que irão adicionar os atributos da classe
+    /** @brief Executa a adição das coordenadas de dois pontos
+    *   @param p1 Ponto contendo as coordenadas que irão somar-se aos estados da classe
     *   @return Ponto resultante da adição
     */
     Ponto soma(Ponto p1);
     /** @brief Executa a subtração de pontos
-    *   @param p1 Ponto contendo as coordenadas que irão subtrair os atributos da classe
+    *   @param p1 Ponto contendo as coordenadas que irão subtrair-se aos estados da classe
     *   @return Ponto resultante da subtração
     */
     Ponto sub(Ponto p1);
@@ -52,9 +52,9 @@ public:
     *   @return Distância do ponto até a origem
     */
     float norma(void);
-    /** @brief Desloca o ponto para uma nova posição
-        @param a Valor que será somado a coordenada x do ponto
-        @param b Valor que será somado a coordenada y do ponto
+    /** @brief Desloca o ponto para uma nova posição no plano cartesiano
+        @param a Valor que será somado à coordenada x do ponto
+        @param b Valor que será somado à coordenada y do ponto
     */
     void translada(float a, float b);
     /** @brief Exibe as coordenadas x e y do ponto no formato <i>(x, y)</i>
